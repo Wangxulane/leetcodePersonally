21dfba684ee0e88503efd7fb51a78d7598a87f39
Wangxulane <1564334242@qq.com>
2024-03-28 22:45:11 +0800

    第一次测试通过

--- 3.28/no_1997.c
+++ 3.28/no_1997.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 
 int firstDayBeenInAllRooms(int* nextVisit, int nextVisitSize) {
-    
+    int day = 0;
+    int* count;
+    int i;
+    count = (int*)malloc(sizeof(int) * nextVisitSize);
+    int visit = 0; // 访问的位置编号
+
+    for (i = 0; i < nextVisitSize; i++) {
+        count[i] = 0;
+    }
+
+    while (visit < nextVisitSize-1) {
+        day += 1;
+        count[visit] += 1;
+        if (count[visit] % 2 == 1) {
+            visit = nextVisit[visit];
+        } else {
+            visit = (visit + 1) % nextVisitSize;
+        }
+    }
+    free(count);
+    return day;
 }
