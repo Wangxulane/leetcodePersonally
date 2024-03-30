98666ca3ae48270e7df2112a31b54fb82521a044
Wangxulane <1564334242@qq.com>
2024-03-30 14:35:20 +0800

    力扣官方题解

---
+++ 3.30/no_2952.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int minimumAddedCoins(int* coins, int coinsSize, int target) {
+    coins=sortcoins(coins,coinsSize);
+    
+}
+
+// 冒泡排序
+int* sortcoins(int* coins,int coinsSize){
+    int temp;
+    for(int i=0;i<coinsSize-1;i++){
+        for(int j=0;j<coinsSize-1-i;j++){
+            if(coins[j]>coins[j+1]){
+                temp=coins[j];
+                coins[j]=coins[j+1];
+                coins[j+1]=temp;
+            }
+        }
+    }
+    return coins;
+}
